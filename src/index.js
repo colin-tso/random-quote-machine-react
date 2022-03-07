955814df101ebfd5cdd9f1d51387c9ee6956506e
GrandTheftGelato <colintso@hotmail.com>
2022-03-07 18:54:28 +1100

    Fix mobile page overflow

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Then we set the value in the --height custom property to the root of the document
+document.documentElement.style.setProperty("--page-height", `${window.innerHeight}px`);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('quote-box')
 );
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  document.documentElement.style.setProperty("--page-height", `${window.innerHeight}px`);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
